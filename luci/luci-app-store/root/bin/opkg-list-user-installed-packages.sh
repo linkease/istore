2bec398cc5ccb0973ddcc26d8e0d1d14022d36aa
6ang <6ang996@gmail.com>
2021-11-11 22:20:53 +0800

    add backup to dir support

---
+++ luci/luci-app-store/root/bin/opkg-list-user-installed-packages.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/awk -f
+BEGIN {
+	D="cd /overlay/upper/usr/lib/opkg/info&&"
+	C=D"ls *.list"
+	S="sort -n"
+	while(C|getline>0) {
+		P=substr(F=$1,1,length($1)-5)
+		J=D"du -sk $(cat "F")"
+		s=0
+		while(J|getline>0) s+=$1
+		close(J)
+		t+=s
+		print s" "P|S
+	}
+	close(S)
+}
